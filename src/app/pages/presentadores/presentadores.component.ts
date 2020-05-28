c41c7bf708343f94ae695e78f30e0d49d9915d92
Teresa Margarita Orozco Bon <torozcobon@guanajuato.gob.mx>
2020-05-27 21:51:53 -0500

    ADD Programa de activdades, Comite y Personalidades; CHANGE imagenes de carrusel

---
+++ src/app/pages/presentadores/presentadores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-presentadores',
+  templateUrl: './presentadores.component.html',
+  styleUrls: ['./presentadores.component.css']
+})
+export class PresentadoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
