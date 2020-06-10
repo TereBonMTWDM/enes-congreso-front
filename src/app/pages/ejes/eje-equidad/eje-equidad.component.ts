9c0feef5527498e19f9f33c694f19d7ef5c1a787
Teresa Margarita Orozco Bon <torozcobon@guanajuato.gob.mx>
2020-06-10 15:14:07 -0500

    ADD popup de ejes; ADD sector de virtual; CHANGE orden menu; CHANGE comites

---
+++ src/app/pages/ejes/eje-equidad/eje-equidad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eje-equidad',
+  templateUrl: './eje-equidad.component.html',
+  styleUrls: ['./eje-equidad.component.css']
+})
+export class EjeEquidadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/ejes/eje-procesos/eje-procesos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eje-procesos',
+  templateUrl: './eje-procesos.component.html',
+  styleUrls: ['./eje-procesos.component.css']
+})
+export class EjeProcesosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
