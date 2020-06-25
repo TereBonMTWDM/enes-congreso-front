78a9f12c9cab4b3ee20bab6042246c2190735736
Teresa Margarita Orozco Bon <torozcobon@guanajuato.gob.mx>
2020-06-25 17:13:57 -0600

    ADVANCE; ADD Fechas importantes

---
+++ src/app/pages/enes/enes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enes',
+  templateUrl: './enes.component.html',
+  styleUrls: ['./enes.component.css']
+})
+export class EnesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
